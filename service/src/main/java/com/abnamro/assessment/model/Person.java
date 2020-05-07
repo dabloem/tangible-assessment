11bbb316aa3e57e79d08ecd018c91d672db106be
Duncan <dabloem@gmail.com>
2020-05-07 10:02:17 +0000

    assignment one

--- service/src/main/java/com/abnamro/assessment/model/Person.java
+++ service/src/main/java/com/abnamro/assessment/model/Person.java
@@ -2,12 +2,59 @@ package com.abnamro.assessment.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  * Person
  */
+@Entity
 public class Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Size(min = 2)
+    @Column(unique = true)
     private String name;
+
+    @NotNull
     private LocalDate birthDate;
-    
+
+    public Person(String name, LocalDate birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
